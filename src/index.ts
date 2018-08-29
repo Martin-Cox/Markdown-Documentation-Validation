a4dc7036d12738ce0fe5c93d4b27cdfbf577aa14
Martin Cox <martin.cox1994@gmail.com>
2018-08-29 09:03:29 +0100

    Read files from a directory

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,53 @@
 import fileSystem from "fs-extra";
 
+import * as yargs from "yargs";
+
 import { Rule } from "./rule";
 import { rules } from "./rules";
 
-const demoMarkdown = fileSystem.readFileSync("example/example.md", "utf8");
-const violations:{ line: number, rule: Rule}[] = [];
+// Set up the command line arguments
+const argv = yargs
+	.wrap(yargs.terminalWidth())
+	.usage("Validates markdown documentation against a set of rules.")
+	.option("directory", {
+			default: "example",
+			defaultDescription: "The directory containing example documentation.",
+			description: "The directory to read markdown documentation from."
+	}).argv;
 
-rules.forEach((rule) => {
-	const matches = rule.matches(demoMarkdown);
+const markdownFileRegex = /\.md$/g;
+const directory = argv.directory;
 
-	for (let i = 0; i < matches; i++) {
-		violations.push({ line: -1, rule });
-	}
-});
+// Read all of the files in the directory.
+fileSystem.readdir(directory).then((fileNames: string[]) => {
+	// Filter out any files that are not .md files.
+	return fileNames.filter((fileName) => markdownFileRegex.test(fileName));
+}).then((fileNames: string[]) => {
+	fileNames.forEach((fileName) => {
+		const markdownString = fileSystem.readFileSync(`${directory}/${fileName}`, "utf8");
 
-if (violations) {
-	console.log("------------------------------------------------");
-	console.log("demo.md");
-	console.log(`${violations.length} warnings:`);
-	violations.forEach((violation) => {
-		const violationText = violation.rule.getViolationText();
-		console.log(`Line ${violation.line}: ${violation.rule.name}${violationText ? ` - ${violationText}` : ""}`);
+		const violations:{ line: number, rule: Rule}[] = [];
+
+		rules.forEach((rule) => {
+			const matches = rule.matches(markdownString);
+	
+			for (let i = 0; i < matches; i++) {
+				violations.push({ line: -1, rule });
+			}
+		});
+	
+		if (violations) {
+			console.log("------------------------------------------------");
+			console.log(fileName);
+			console.log(`${violations.length} warnings:`);
+			violations.forEach((violation) => {
+				const violationText = violation.rule.getViolationText();
+				console.log(`Line ${violation.line}: ${violation.rule.name}${violationText ? ` - ${violationText}` : ""}`);
+			});
+			console.log("------------------------------------------------");
+		}
 	});
-	console.log("------------------------------------------------");
-}
+});
 
 //TODO:
 // - Read files from a given directory (yargs?)
