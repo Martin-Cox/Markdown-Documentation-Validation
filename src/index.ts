ba1c9cca1666781b27e6cf0b61588146fe631150
Martin Cox <martin.cox1994@gmail.com>
2018-08-31 17:11:16 +0100

    Load rules from a customisable context file

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as lineReader from "line-reader";
 import * as yargs from "yargs";
 
 import { Rule } from "./rule";
-import { rules } from "./rules";
+import { Rules } from "./rules";
 
 // Set up the command line arguments
 const argv = yargs
@@ -13,45 +13,54 @@ const argv = yargs
 			default: "example",
 			defaultDescription: "The directory containing example documentation.",
 			description: "The directory to read markdown documentation from."
+	})
+	.option("config", {
+			description: "The location of the config file."
 	}).argv;
 
 const directory = argv.directory;
+const config = argv.config;
+const rules = new Rules(config);
 
-// Read all of the files in the directory.
-fileSystem.readdir(directory).then((fileNames: string[]) => {
-	// Filter out any files that are not .md files.
-	return fileNames.filter((fileName) => /\.md$/g.test(fileName));
-}).then((fileNames: string[]) => {
-	fileNames.forEach((fileName) => {
-		const violations:{ line: number, rule: Rule}[] = [];
-		let lineNumber = 1;
-		
-		// Poor typings mean we have to cast eachLine as any.
-		(lineReader.eachLine as any)(`${directory}/${fileName}`, (line: string, last: boolean, callBack: (done: boolean) => void) => {
-			rules.forEach((rule) => {
-				const matches = rule.matches(line);
-		
-				for (let i = 0; i < matches; i++) {
-					violations.push({ line: lineNumber, rule });
+rules.buildRules().then(() => {
+	// Read all of the files in the directory.
+	fileSystem.readdir(directory).then((fileNames: string[]) => {
+		// Filter out any files that are not .md files.
+		return fileNames.filter((fileName) => /\.md$/g.test(fileName));
+	}).then((fileNames: string[]) => {
+		fileNames.forEach((fileName) => {
+			const violations:{ line: number, rule: Rule}[] = [];
+			let lineNumber = 1;
+			
+			// Poor typings mean we have to cast eachLine as any.
+			(lineReader.eachLine as any)(`${directory}/${fileName}`, (line: string, last: boolean, callBack: (done: boolean) => void) => {
+				rules.rules.forEach((rule) => {
+					const matches = rule.matches(line);
+			
+					for (let i = 0; i < matches; i++) {
+						violations.push({ line: lineNumber, rule });
+					}
+				});
+
+				lineNumber++;
+
+				callBack(!last);
+			}, () => {
+				if (violations.length > 0) {
+					console.log(fileName);
+					console.log(`${violations.length} warnings:`);
+					violations.forEach((violation) => {
+						const violationText = violation.rule.getViolationText();
+						console.log(`Line ${violation.line}: ${violation.rule.name}${violationText ? ` - ${violationText}` : ""}`);
+					});
+					console.log("------------------------------------------------");
 				}
 			});
-
-			lineNumber++;
-
-			callBack(!last);
-		}, () => {
-			if (violations.length > 0) {
-				console.log(fileName);
-				console.log(`${violations.length} warnings:`);
-				violations.forEach((violation) => {
-					const violationText = violation.rule.getViolationText();
-					console.log(`Line ${violation.line}: ${violation.rule.name}${violationText ? ` - ${violationText}` : ""}`);
-				});
-				console.log("------------------------------------------------");
-			}
 		});
 	});
 });
 
 //TODO:
-// - Add some mechanism for reading in rules from an external file so people can customise their rules
+// - Remove restriction on .md files
+// - Expand rules to determine whether they apply on a per line/per file basis
+// - Better validation of config files
--- src/rules.ts
+++ src/rules.ts
@@ -1,8 +1,97 @@
+import fileSystem from "fs-extra";
+
 import { Rule } from "./rule";
 
-export const rules: Rule[] = [
-	new Rule("No AP", "\\bAP\\b", { suggestion: "Awesome Product", keyword: "AP" }),
-	new Rule("File Name Casing", "\\\.JSON", { caseSensitive: true, suggestion: ".json" }),
-	new Rule("Strict Equality", "==", { suggestion: "===" }),
-	new Rule("No buh", "buh")
-];
+// export const rules: Rule[] = [
+// 	new Rule("No AP", "\\bAP\\b", { suggestion: "Awesome Product", keyword: "AP" }),
+// 	new Rule("File Name Casing", "\\\.JSON", { caseSensitive: true, suggestion: ".json" }),
+// 	new Rule("Strict Equality", "==", { suggestion: "===" }),
+// 	new Rule("No buh", "buh")
+// ];
+
+/**  */
+interface ConfigurationRule {
+	/**  */
+	name: string;
+
+	/**  */
+	regex: string;
+
+	/**  */
+	options?: {
+		/** A suggested resolution to resolve rule violations. */
+		suggestion?: string;
+
+		/** The keyword that triggered the rule violation. */
+		keyword?: string;
+
+		/** Whether the regex should be case sensitive or not. */
+		caseSensitive?: boolean;
+
+		/** The justification for this rule (if not immediately obvious). */
+		justification?: string;
+	}
+}
+
+interface ConfigurationOptions {
+	rules?: ConfigurationRule[];
+};
+
+
+export class Rules {
+	/** The collection of individual rules. */
+	private _rules: Rule[] = [];
+	
+	/** The location of the configuration file. */
+	private _configLocation: string | undefined;
+
+	/**
+	 * Creates the Rules collection.
+	 * @param configLocation The optional location of the configuration file.
+	 */
+	public constructor(configLocation?: string) {
+		this._configLocation = configLocation;
+
+		// TODO: Convert all "system" rules to config file equivalents.
+		this._rules = this._rules.concat([
+			new Rule("No TODO", "\\bTODO\\b"),
+			new Rule("File Name Casing", "\\\.JSON", { caseSensitive: true, suggestion: ".json" }),
+			new Rule("Strict Equality", "==", { suggestion: "===" }),
+			new Rule("Strict Equality", "!=", { suggestion: "!==" }),
+			new Rule("Context Format", "\\$\\$.*?\\$\\$", { suggestion: "\\$\\$fldvalue\\$\\$", keyword: "$$context$$", justification: "the markdown processor incorrectly formats contexts when the dollar signs are not escaped"})
+		]);
+	}
+
+	/** Gets the list of rules. */
+	public get rules(): Rule[] {
+		return this._rules;
+	}
+
+	/**
+	 * 
+	 * @returns
+	 */
+	public buildRules(): Promise<void> {
+		// If no configuration file was provided then simply return a resolved promise.
+		if (!this._configLocation) {
+			return Promise.resolve();
+		}
+
+
+		// TODO: Typings for config
+		// TODO: Convert to Promises/async
+		const config: ConfigurationOptions = JSON.parse(fileSystem.readFileSync(this._configLocation, "utf8"));
+		config;
+
+		// Add any rules from the options (if there are any).
+		if (config.rules) {
+			config.rules.forEach((configRule) => {
+				this._rules.push(new Rule(configRule.name, configRule.regex, configRule.options));
+			});
+		}
+
+		return Promise.resolve();
+	}
+}
+
+//TODO: Can probably have buildRules() return a promise containing the rules collection and chain that through in index.ts
